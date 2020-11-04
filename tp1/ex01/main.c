c032ee049780c73c855eab804cc3362b67db5acc
huggo <hutricot@student.42.fr>
2020-11-04 19:22:38 +0100

    c est deja smieux

--- tp1/ex01/main.c
+++ tp1/ex01/main.c
@@ -4,14 +4,15 @@ int main()
 {
    //set toutes les pin B en input
    DDRB = (1 << PB1);
-   //set com1B1 et com1B0 COM1A0
+   //set Icr1 pour faire des boucle de 1sec
    ICR1 = 31249;
+   //set ocr1a pour etre allmuer 10% du temps de boucle
    OCR1A = 3124;
+   //set TCCR1A sur clear on compare match (en fast pwm)
    TCCR1A |= (1 << COM1A1);
-   TCCR1A |= (1 << COM1B1);
    //TCCR1A = (1 << COM1A1);
    //TCCR1A = (1 << WGM10);
-   //mode 14
+   //set le mode 14
    TCCR1A |= (1 << WGM11);
    TCCR1B |= (1 << WGM12);
    TCCR1B |= (1 << WGM13);
@@ -24,4 +25,5 @@ int main()
    {
 
    }
+   //pour plus d info :
 }
