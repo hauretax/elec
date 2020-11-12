3da74f68804d375db7f19139e9c913b2f6561afd
huggo <hutricot@student.42.fr>
2020-11-12 16:14:22 +0100

    tp2

---
+++ tp2/ex00/main.c
@@ -0,0 +1,27 @@
+#include <avr/io.h>
+
+#define  interrupt_handler(vector, ...) \
+         void vector (void) __attribute__ ((signal)) __VA_ARGS__; \
+         void vector (void)
+
+interrupt_handler(INT1_vect){
+   PORTB = 0b1111;
+}
+
+int main()
+{
+   //set toutes les pin B en output
+   DDRB |= (1 << PB1);
+   DDRB |= (1 << PB2);
+   DDRB |= (1 << PB3);
+   DDRB |= (1 << PB0);
+   //activer linterupte!!!!!
+   SREG = 0b10000000;
+   //set maske 
+   EIMSK = (1 << INT1);
+
+   while(1)
+   {
+   }
+   //pour plus d info :
+}
---
+++ tp2/ex01/main.c
@@ -0,0 +1,68 @@
+#include <avr/io.h>
+
+#define  interrupt_handler(vector, ...) \
+         void vector (void) __attribute__ ((signal)) __VA_ARGS__; \
+         void vector (void)
+
+volatile int i;
+
+interrupt_handler(TIMER0_COMPA_vect){    
+   if(ICR1 == OCR1A)
+      i = 0;
+   if(!(OCR1A))
+      i = 1;
+   if(i){
+         OCR1A ++;
+   }
+   else{
+         OCR1A--;
+   }
+}
+
+int main()
+{
+   i=1;
+   //set toutes les pin B en output
+   DDRB |= (1 << PB1);
+   DDRB |= (1 << PB2);
+   DDRB |= (1 << PB3);
+   DDRB |= (1 << PB0);
+
+
+   ICR1 = 125;
+   //set ocr1a pour etre allmuer 10% du temps de boucle
+   OCR1A=0;
+   //set TCCR1A sur clear on compare match (en fast pwm)
+   TCCR1A |= (1 << COM1A1);
+   //TCCR1A = (1 << COM1A1);
+   //TCCR1A = (1 << WGM10);
+   //set le mode 14
+   TCCR1A |= (1 << WGM11);
+   TCCR1B |= (1 << WGM12);
+   TCCR1B |= (1 << WGM13);
+   //set le presclaer
+   //TCCR1B |= (1 << CS12);
+   TCCR1B |= (1 << CS10);
+
+   //set toggle
+   TCCR0A |= (1 << COM0B0);
+   //set le mode 2
+   TCCR0A |= (1 << WGM01);
+   TCCR0A |= (1 << WGM00);
+   TCCR0B |= (1 << WGM02);
+   //activer linterupte!!!!!
+   SREG = 0b10000000;
+   //set maske 
+   TIMSK0 = (1 << INT1);
+   //set le max
+   OCR0A = 255;
+   //set clock timer a 1024
+   TCCR0B |= (1 << CS02);
+   TCCR0B |= (1 << CS00);
+
+
+   while(1)
+   {
+   }
+   //pour plus d info :
+}
